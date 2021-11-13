7ffe70990f8c1b5010d28960601405a8dc80fefd
Xavier <xavier.luzuriaga@gmail.com>
2021-11-13 16:57:35 -0300

    first time

---
+++ server/OrderUp/src/test/java/io/orderup/OrderUpApplicationTests.java
@@ -0,0 +1,13 @@
+package io.orderup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
