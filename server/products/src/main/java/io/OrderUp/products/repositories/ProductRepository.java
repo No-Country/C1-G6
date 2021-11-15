6b4ff4dc8265f950c824b680fa99fbe78ce9a17f
Xavier <xavier.luzuriaga@gmail.com>
2021-11-15 18:18:26 -0300

    back to original al node_modules

--- server/products/src/main/java/io/OrderUp/products/repositories/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package io.OrderUp.products.repositories;
-
-import io.OrderUp.products.models.Product;
-import org.springframework.data.repository.CrudRepository;
-import java.util.Optional;
-
-public interface ProductRepository extends CrudRepository<Product, String>{
-}
