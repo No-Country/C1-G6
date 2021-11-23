bb6bea1bf26da4e69cff0b0727bf12dc2a6a819e
Tomás Reinhardt <tomas_reinhardt@hotmail.com>
2021-11-23 15:28:08 -0300

    Jquery

--- client/OrderUp/src/app/app.module.ts
+++ client/OrderUp/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { routing, appRoutingProviders } from './app.routing';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { StockComponent } from './components/stock/stock.component';
@@ -26,6 +27,7 @@ import { RegisterComponent } from './components/register/register.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     routing
   ],
   providers: [appRoutingProviders],
