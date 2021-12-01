cf1cd8f88b28cca1ef4aa17a065bd59655b7ac62
Nicolas-85 <nicolasmartinalvarez85@gmail.com>
2021-12-01 14:48:31 -0300

    Maquetación Stock y Mesas.

--- client/OrderUp/src/app/components/stock/stock.component.ts
+++ client/OrderUp/src/app/components/stock/stock.component.ts
@@ -8,7 +8,37 @@ import { Component, OnInit } from '@angular/core';
 export class StockComponent implements OnInit {
 
   constructor() { }
-
+  public products = [
+    {
+      name: "milanesa",
+      stock: true
+    },
+    {
+      name: "milanesa completa",
+      stock: true
+    },
+    {
+      name: "hamburguesa",
+      stock: true
+    },
+    {
+      name: "coca-cola",
+      stock: true
+    },
+    {
+      name: "agua",
+      stock: true
+    },
+    {
+      name: "papas fritas",
+      stock: true
+    },
+    {
+      name: "chocolina",
+      stock: true
+    }
+  ];
+  
   ngOnInit(): void {
   }
 
